5b101e14ac1fe7e188cb1a26469c6bfc012f556f
Gabrielle <gfulk@trinity.edu>
2021-08-06 12:58:30 -0500

    Change spacing and adjust project images

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,25 +1,44 @@
 import React from "react"
 import SpacerColumn from "../components/SpacerColumn"
 import websiteSS from "../assets/websiteSS.jpg"
-import ProjectCard from "../components/ProjectCard"
 
 const websiteDesc = ("This website is my first publicly displayed front-end project. I built it using ReactJS and Tailwind. As my portfolio grows and my creative vision evolves I plan to continue to update it.")
 
 const ProjectsPage = () => {
     return (
-        <div className="flex flex-row flex-wrap pb-32">
+        <div className="flex flex-row flex-wrap m-auto pb-32">
             <SpacerColumn/>
             <div className="flex flex-col">
                 <div className="page-heading w-24 pb-8">
                     Projects
                 </div>
             </div>
-            {/* <SpacerColumn/> */}
-            <div className="flex flex-row flex-wrap">
-                <SpacerColumn/>
-                <ProjectCard image={websiteSS} name="gabriellefulk.dev" description={websiteDesc} btnName="GITHUB" btnLink="https://github.com/GabrielleFulk/GabrielleFulk.github.io"/>
-                <ProjectCard image={websiteSS} name="gabriellefulk.dev" description={websiteDesc} btnName="GITHUB" btnLink="https://github.com/GabrielleFulk/GabrielleFulk.github.io"/>
+            <SpacerColumn/>                
+            <div className="flex flex-col flex-1 h-min w-52 space-y-4 text-title px-7">
+                <img className="max-h-72 self-center rounded-xl border-8 border-water shadow-md" src={websiteSS} alt=""/>
+                <div className="text-lg font-bold">
+                    gabriellefulk.dev
+                </div>
+                <div className="">
+                    {websiteDesc}
+                </div>
+                <button className= "bg-water py-1 px-2 w-20 h-6 rounded-md border-0 shadow-md tracking-tighter text-sm font-bold self-center" href="https://github.com/GabrielleFulk/GabrielleFulk.github.io">
+                    GITHUB 
+                </button>
             </div>
+            <div className="flex flex-col flex-1 h-min w-52 space-y-4 text-title px-7">
+                <img className="max-h-72 self-center rounded-xl border-8 border-water shadow-md" src={websiteSS} alt=""/>
+                <div className="text-lg font-bold">
+                    gabriellefulk.dev
+                </div>
+                <div className="">
+                    {websiteDesc}
+                </div>
+                <button className= "bg-water py-1 px-2 w-20 h-6 rounded-md border-0 shadow-md tracking-tighter text-sm font-bold self-center" href="https://github.com/GabrielleFulk/GabrielleFulk.github.io">
+                    GITHUB 
+                </button>
+            </div>
+            <SpacerColumn/>
         </div>
     )
 }
--- src/pages/ResumePage.js
+++ src/pages/ResumePage.js
@@ -5,23 +5,23 @@ import resumeDownload from "../assets/resumeDownload.pdf"
 
 const ResumePage = () => {
     return (
-        <div className="flex flex-row flex-wrap pb-10">
+        <div className="flex flex-row flex-wrap m-auto pb-10">
             <SpacerColumn/>
             <div className="flex flex-col">
                 <div className="page-heading w-24 pb-8">
                     Resume
                 </div>
             </div>
-            <div className="flex flex-row justify-center">
+            {/* <div className="flex flex-row justify-center m-auto pr-3"> */}
                 <SpacerColumn/>
-                <div className="flex flex-col items-center">
-                    {/* <img className="transform scale-75 -mt-24 -mb-20 rounded-xl border-8 border-water shadow-md" src={resume} alt=""/> */}
-                    <img className="object-scale-down h-full w-96 rounded-lg border-8 border-water shadow-md" src={resume} alt=""/>
+                <div className="flex flex-col items-center space-y-3">
+                    <img className="h-full w-full rounded-lg border-8 border-water shadow-md" src={resume} alt=""/>
                     <a href={resumeDownload} download="GabrielleFulkResume">
                         <button className= "bg-water py-1 px-2 rounded-md border-0 shadow-md tracking-tighter text-sm font-bold">DOWNLOAD</button>
                     </a>
                 </div>
-            </div>
+            {/* </div> */}
+            <SpacerColumn/>
         </div>
     )
 }
